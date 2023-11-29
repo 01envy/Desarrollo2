8e20333fb084e9646c21ada20430f389868f3281
carlos <carlosalday15mail.com>
2023-11-28 21:36:32 -0300

    agrege una linea

--- panel.php
+++ panel.php
@@ -461,6 +461,9 @@ $con = conectar();
                     <div class="mb-2 enlacesfooter">
                         <a class="text-secondary text-decoration-none" href="#">Instagram</a>
                     </div>
+                    <div class="mb-2 enlacesfooter">
+                        <a class="text-secondary text-decoration-none" href="#">Instagram</a>
+                    </div>
                 </div>
     
                 <!--Columna 4-->
